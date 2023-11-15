53e13bf6f3c4b3062c79c4bd6ce82e41afbd236e
Oleksii <oleksiy678@gmail.com>
2023-11-15 19:32:56 +0000

    basic functionality added for creating a new profile and cancellation of creating of a new profile

--- src/CreateProfileWindow.java
+++ src/CreateProfileWindow.java
@@ -1,10 +1,24 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 public class CreateProfileWindow extends Application {
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button cancelButton;
+
     public void show() {
         Application.launch();
     }
@@ -19,5 +33,20 @@ public class CreateProfileWindow extends Application {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void addButtonCLicked(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.getDialogPane().setContent( new Text("Congratulations!"
+                + "You added a new profile with username: "
+                + usernameTextField.getText()));
+        alert.showAndWait();
+        Stage stage = (Stage) addButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void cancelButtonClicked(ActionEvent actionEvent) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
