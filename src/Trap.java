63cb4d5304866fd2a9dd0face44d033448471526
Oleksii <oleksiy678@gmail.com>
2023-11-20 11:17:08 +0000

    Trap class added

---
+++ src/Trap.java
@@ -0,0 +1,22 @@
+public class Trap extends Path {
+    private boolean isActivated;
+
+    /**
+     * Create a new Trap tile with specified x and y coordinates and stating if it is walkable or not
+     * @param posX The x coordinate.
+     * @param posY The y coordinate.
+     * @param isWalkable Is this tile walkable or not.
+     */
+    public Trap(int posX, int posY, boolean isWalkable, boolean isActivated) {
+        super(posX, posY, isWalkable);
+        this.isActivated = isActivated;
+    }
+
+    /**
+     * A function that tells if the trap is activated.
+     * @return True if the trap is activated, false otherwise.
+     */
+    public boolean isActivated() {
+        return this.isActivated;
+    }
+}
