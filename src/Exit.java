1ccb4866659c30e2bec81ada33f720e434ed0636
Oleksii <oleksiy678@gmail.com>
2023-11-20 13:09:18 +0000

    Exit class added

---
+++ src/Exit.java
@@ -0,0 +1,11 @@
+public class Exit extends Path {
+    /**
+     * Create a new Exit tile with specified x and y coordinates and stating if it is walkable or not
+     * @param posX The x coordinate.
+     * @param posY The y coordinate.
+     * @param isWalkable Is this tile walkable or not.
+     */
+    public Exit(int posX, int posY, boolean isWalkable) {
+        super(posX, posY, isWalkable);
+    }
+}
