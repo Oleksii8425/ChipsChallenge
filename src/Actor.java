d7e69e8acc4bc90d1eeef586aefe3dc42006bd78
Morgan6Golygus <150295953+Morgan6Golygus@users.noreply.github.com>
2023-11-20 10:09:20 +0000

    Update Actor.java

--- src/Actor.java
+++ src/Actor.java
@@ -1,45 +1,95 @@
+/**
+ * Represents an actor with position and distance attributes.
+ */
 public class Actor {
     private int posX;
     private int posY;
     private int distance;
 
+    /**
+     * Constructor for the Actor class.
+     *
+     * @param posX     The initial X-coordinate position of the actor.
+     * @param posY     The initial Y-coordinate position of the actor.
+     * @param distance The distance attribute of the actor.
+     */
     public Actor(int posX, int posY, int distance) {
         this.posX = posX;
         this.posY = posY;
         this.distance = distance;
     }
+       
+    /**
+     * Gets the X-coordinate position of the actor.
+     *
+     * @return The X-coordinate position.
+     */
+
 
     public int getPosX() {
         return posX;
     }
+        /**
+     * Sets the X-coordinate position of the actor.
+     *
+     * @param x The new X-coordinate position.
+     */
+
 
     public void setPosX(int x) {
         this.posX = x;
-    }
+    } 
+     /**
+     * Gets the Y-coordinate position of the actor.
+     *
+     * @return The Y-coordinate position.
+     */
 
     public int getPosY() {
         return posY;
     }
+          /**
+     * Sets the Y-coordinate position of the actor.
+     *
+     * @param x The new Y-coordinate position.
+     */
 
     public void setPosY(int y) {
         this.posY = y;
     }
+        /**
+     * Gets the direction of the actor's movement.
+     *
+     * @return The direction of movement.
+     */
 
     public String getDirection() {
-        // Placeholder method, replace with actual logic to get direction
-        return "Direction"; // Example placeholder return value
+        
+        return "Direction"; 
     }
+        
+    /**
+     * Sets the direction of the actor's movement.
+     *
+     * @param direction The new direction of movement.
+     */
+
 
     public void setDirection(String direction) {
-        // Placeholder method, replace with actual logic to set direction
+       
     }
+       
+    /**
+     * Gets the distance attribute of the actor.
+     *
+     * @return The distance attribute.
+     */
+
 
     public int getDistance() {
         return distance;
     }
 
-    public void move(String direction) {
-        // Placeholder method, replace with actual logic to move based on direction
-        System.out.println("Moving in direction: " + direction);
-    }
+    public void move(String direction) {}
+
 }
