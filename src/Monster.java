e3d40377fa3eae7e20c420d9809bd013a279ca52
Morgan6Golygus <150295953+Morgan6Golygus@users.noreply.github.com>
2023-11-22 10:12:26 +0000

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -1,6 +1,11 @@
+/**
+ * A class representing monster class which extends actor
+ * @author Morgan Greenway, Anna Hardy
+ * @version 1.0
+ */
+
 public class Monster extends Actor {
     public Monster(int posX, int posY, int distance) {
         super(posX, posY, distance);
     }
-
 }
