83e363390a9c79e7ef391f16c038b01f5ada0ceb
Morgan6Golygus <150295953+Morgan6Golygus@users.noreply.github.com>
2023-12-04 11:08:16 +0000

    Add files via upload

---
+++ Bug.java
@@ -0,0 +1,57 @@
+package com.example.playertest;
+
+public class Bug extends Monster {
+    private boolean followsLeftEdge;
+
+    public Bug(int posX, int posY, boolean followsLeftEdge) {
+        super(posX, posY, 1); // Assuming distance/speed for Bug is set to 1
+        this.followsLeftEdge = followsLeftEdge;
+    }
+
+    public void followEdge() {
+
+        int currentX = getPosX();
+        int currentY = getPosY();
+
+        // Assume there are methods available to check available paths (isPath(), isBlocked(), etc.)
+
+        if (followsLeftEdge) {
+            // Try to follow the left edge
+            if (isPath(currentX - 1, currentY)) {
+                // Move left
+                setPosX(currentX - 1);
+                setPosY(currentY);
+            } else if (isPath(currentX, currentY - 1)) {
+                // Move up
+                setPosX(currentX);
+                setPosY(currentY - 1);
+            } else if (isPath(currentX + 1, currentY)) {
+                // Move right
+                setPosX(currentX + 1);
+                setPosY(currentY);
+            } else {
+                // Move down
+                setPosX(currentX);
+                setPosY(currentY + 1);
+            }
+        } else {
+            if (isPath(currentX + 1, currentY)) {
+                // Move right
+                setPosX(currentX + 1);
+                setPosY(currentY);
+            } else if (isPath(currentX, currentY - 1)) {
+                // Move up
+                setPosX(currentX);
+                setPosY(currentY - 1);
+            } else if (isPath(currentX - 1, currentY)) {
+                // Move left
+                setPosX(currentX - 1);
+                setPosY(currentY);
+            } else {
+                // Move down
+                setPosX(currentX);
+                setPosY(currentY + 1);
+            }
+        }
+    }
+}
