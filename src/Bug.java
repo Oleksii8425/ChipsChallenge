fdb9c4f9cabb9f48d665048c08467f57c8e889b7
Morgan6Golygus <150295953+Morgan6Golygus@users.noreply.github.com>
2023-12-05 17:34:48 +0000

    Update Bug.java

--- src/Bug.java
+++ src/Bug.java
@@ -1,15 +1,26 @@
 package com.example.playertest;
-
+/**
+        * This class represents a Bug which is a type of Monster.
+        * It .
+        * @author .
+ */
 public class Bug extends Monster {
+    private final static int BUG_SPEED = 1;
+    private final static int MOVE_ONE_POSITION = 1;
     private boolean followsLeftEdge;
 
+    /**
+     * This creates a Bug, with a position
+     * @param posX
+     * @param posY
+     * @param followsLeftEdge
+     */
     public Bug(int posX, int posY, boolean followsLeftEdge) {
-        super(posX, posY, 1); // Assuming distance/speed for Bug is set to 1
+        super(posX, posY, BUG_SPEED); // Assuming distance/speed for Bug is set to 1
         this.followsLeftEdge = followsLeftEdge;
     }
 
     public void followEdge() {
-
         int currentX = getPosX();
         int currentY = getPosY();
 
@@ -17,40 +28,40 @@ public class Bug extends Monster {
 
         if (followsLeftEdge) {
             // Try to follow the left edge
-            if (isPath(currentX - 1, currentY)) {
+            if (isPath(currentX - MOVE_ONE_POSITION, currentY)) {
                 // Move left
-                setPosX(currentX - 1);
+                setPosX(currentX - MOVE_ONE_POSITION);
                 setPosY(currentY);
-            } else if (isPath(currentX, currentY - 1)) {
+            } else if (isPath(currentX, currentY - MOVE_ONE_POSITION)) {
                 // Move up
                 setPosX(currentX);
-                setPosY(currentY - 1);
-            } else if (isPath(currentX + 1, currentY)) {
+                setPosY(currentY - MOVE_ONE_POSITION);
+            } else if (isPath(currentX + MOVE_ONE_POSITION, currentY)) {
                 // Move right
                 setPosX(currentX + 1);
                 setPosY(currentY);
             } else {
                 // Move down
                 setPosX(currentX);
-                setPosY(currentY + 1);
+                setPosY(currentY + MOVE_ONE_POSITION);
             }
         } else {
-            if (isPath(currentX + 1, currentY)) {
+            if (isPath(currentX + MOVE_ONE_POSITION, currentY)) {
                 // Move right
-                setPosX(currentX + 1);
+                setPosX(currentX + MOVE_ONE_POSITION);
                 setPosY(currentY);
-            } else if (isPath(currentX, currentY - 1)) {
+            } else if (isPath(currentX, currentY - MOVE_ONE_POSITION)) {
                 // Move up
                 setPosX(currentX);
-                setPosY(currentY - 1);
-            } else if (isPath(currentX - 1, currentY)) {
+                setPosY(currentY - MOVE_ONE_POSITION);
+            } else if (isPath(currentX - MOVE_ONE_POSITION, currentY)) {
                 // Move left
-                setPosX(currentX - 1);
+                setPosX(currentX - MOVE_ONE_POSITION);
                 setPosY(currentY);
             } else {
                 // Move down
                 setPosX(currentX);
-                setPosY(currentY + 1);
+                setPosY(currentY + MOVE_ONE_POSITION);
             }
         }
     }
